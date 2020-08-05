be0669e5f9402614cb974e7a95d904b1243b6bf0
Solar Flair <jon.s.solari@gmail.com>
2020-08-05 17:59:11 -0400

    file cleanup

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div class="about">
+  <p>This website is an experiment in converting a web app from React to Vue.</p>
+  <p>It pings the <a :href="this.discogs">Discogs API</a> for the lowest current prices for the first five <a :href="this.mtngoats">Mountain Goats</a> albums on vinyl, as well as populating links to each marketplace listing.</p>
+  <p>My code is <a :href="this.github">here</a> if you'd like to take a look.</p>
   </div>
 </template>
+
+<script>
+export default{
+data(){
+  return{
+    discogs: 'https://www.discogs.com/developers',
+    mtngoats: 'https://www.mountain-goats.com/',
+    github: 'https://github.com/jonsolari/tmgvue'
+  }
+}
+}
+</script>
+
+<style scoped>
+.about{
+  margin: 5rem 10%;
+  border: 2px dotted black;
+  border-radius: 10px;
+  padding: 2rem;
+}
+
+a{
+  color: blue;
+  margin: 1rem auto;
+	font-size: 2.3rem;
+}
+</style>
