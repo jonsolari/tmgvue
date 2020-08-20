cffd83ff5fd0404201f60cf46ad04d43eaf76ccd
System Administrator <jon.s.solari@gmail.com>
2020-08-20 13:32:49 -0400

    fixed scraper URL issue

--- src/json/scrapecompare.py
+++ src/json/scrapecompare.py
@@ -30,10 +30,6 @@ def get_detail_data(soup):
         total['url'] = href
         return total
 
-
-# TODO loop over list of releases to search for, update JSON / remove things no longer available 
-
-# make sure account for plain black pressings of BPT and MCB
 releaselist = ["mountain goats zopilote machine lp", "mountain goats sweden lp", "mountain goats nothing for juice lp", "mountain goats full force galesburg lp", "mountain goats coroner's gambit lp", "mountain goats new asian cinema", "mountain goats isopanisad radio hour", "mountain goats devil in the shortwave", "mountain goats black pear tree", "mountain goats satanic messiah", "mountain goats moon colony bloodbath", "mountain goats taboo cassette", "mountain goats come come to the sunset tree", "mountain goats on juhu beach", "mountain goats life of the world to come dvd", "mountain goats songs for pierre chuvin", "mountain goats jam eater blues", "mountain goats all survivors pack", "mountain goats steal smoked fish"]
 
 discogsurls = ['https://api.discogs.com/releases/1601354', 'https://api.discogs.com/releases/1191772', 'https://api.discogs.com/releases/744810', 'https://api.discogs.com/releases/523351', 'https://api.discogs.com/releases/1217804', 'https://api.discogs.com/releases/1625208', 'https://api.discogs.com/releases/1625203', 'https://api.discogs.com/releases/1210894', 'special', 'https://api.discogs.com/releases/1516102','special', 'https://api.discogs.com/releases/2767923', 'https://api.discogs.com/releases/769960', 'https://api.discogs.com/releases/2873969', 'https://api.discogs.com/releases/2732093', 'special', 'https://api.discogs.com/releases/2306617', 'https://api.discogs.com/releases/2797105', 'https://api.discogs.com/releases/3916742']
@@ -62,23 +58,26 @@ high = 1000000
 
 def dcompare(master):
     total = { 'price' : high, 'url' : ''}
+    emptyurl = ''
     i = 0
     while i < len(master):
         temp = discogsget(master[i])
+        emptyurl = temp['url']
         if temp['price'] is not None and float(temp['price']) < float(total['price']):
             total['price'] = temp['price']
             total['url'] = temp['url']
         else:
-            total['url'] = temp['url']
+            pass
         i += 1
     if total['price'] == high:
         total['price'] = None
+        total['url'] = emptyurl
+    print(total)
     return total
 
 i = 0
 while i < len(releaselist):
     ebaydata[i] = ebayget(releaselist[i])
-    print(ebaydata[i])
     i += 1
 
 j = 0
